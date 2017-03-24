79531b74ac2e7b4d0c13ec89e3c50d95e89b2ce7
Blake Feggans <BFeggans@reversevision.com>
2017-03-23 22:34:16 -0700

    init

---
+++ AcmeApp/Acme.Common/OperationResult.cs
@@ -0,0 +1,23 @@
+﻿namespace Acme.Common
+{
+    /// <summary>
+    /// Provides a success flag and message 
+    /// useful as a method return type.
+    /// </summary>
+    public class OperationResult<T>
+    {
+        public OperationResult()
+        {
+        }
+
+        public OperationResult(T success, string message) : this()
+        {
+            this.Success = success;
+            this.Message = message;
+        }
+
+        public T Success { get; set; }
+        public string Message { get; set; }
+    }
+
+}
